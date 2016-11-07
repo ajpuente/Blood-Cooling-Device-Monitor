4bb42254eee9fdc969dac357db1983eac445ac74
Alec Puente <apuente@llnw.com>
2016-11-07 14:32:23 -0700

    Added framework for Bluetooth support for connecting to device

---
+++ app/src/main/java/com/captstone/bloodcoolingmonitor/Constants.java
@@ -0,0 +1,11 @@
+package com.captstone.bloodcoolingmonitor;
+
+/**
+ * Created by alec on 11/7/16.
+ */
+
+public interface Constants {
+
+    public static final int MESSAGE_READ = 1;
+
+}
--- app/src/main/java/com/captstone/bloodcoolingmonitor/MainActivity.java
+++ app/src/main/java/com/captstone/bloodcoolingmonitor/MainActivity.java
@@ -2,10 +2,13 @@ package com.captstone.bloodcoolingmonitor;
 
 import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -14,8 +17,12 @@ import android.widget.TabHost;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 import java.util.Locale;
 import java.util.Set;
+import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends AppCompatActivity {
@@ -27,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
 
     private BluetoothAdapter adapter;
     private Set<BluetoothDevice> pairedDevices;
+    private BluetoothConnectThread connect;
+    private BluetoothConnectedThread connected;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,12 +45,16 @@ public class MainActivity extends AppCompatActivity {
         timerText = (TextView) findViewById(R.id.timerTextView);
         setupButtons();
         setupTabs();
-
-        adapter = BluetoothAdapter.getDefaultAdapter();
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
+        adapter = BluetoothAdapter.getDefaultAdapter();
+        if (adapter == null) {
+            Toast.makeText(getApplicationContext(), getString(R.string.bluetooth_missing),
+                    Toast.LENGTH_LONG).show();
+            return false;
+        }
         getMenuInflater().inflate(R.menu.activity_menu, menu);
         return true;
     }
@@ -69,6 +82,8 @@ public class MainActivity extends AppCompatActivity {
                     Toast.makeText(getApplicationContext(), getString(R.string.bluetooth_already_off_toast),
                             Toast.LENGTH_LONG).show();
                 }
+                return true;
+            case R.id.menu_connect_device:
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
@@ -136,12 +151,12 @@ public class MainActivity extends AppCompatActivity {
 
         TabHost.TabSpec tabSpec = tabHost.newTabSpec("controls");
         tabSpec.setContent(R.id.Controls);
-        tabSpec.setIndicator("Controls");
+        tabSpec.setIndicator(getString(R.string.controls_tab));
         tabHost.addTab(tabSpec);
 
         tabSpec = tabHost.newTabSpec("sensors");
         tabSpec.setContent(R.id.Sensors);
-        tabSpec.setIndicator("Sensors");
+        tabSpec.setIndicator(getString(R.string.sensors_tab));
         tabHost.addTab(tabSpec);
     }
 
@@ -152,5 +167,113 @@ public class MainActivity extends AppCompatActivity {
                         TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis)),
                 TimeUnit.MILLISECONDS.toSeconds(millis) -
                         TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis)));
+    }
+
+    private final Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            byte[] writeBuffer = (byte[]) msg.obj;
+            int begin = msg.arg1;
+            int end = msg.arg2;
+            switch (msg.what) {
+                case Constants.MESSAGE_READ:
+                    String message = new String(writeBuffer);
+                    message = message.substring(begin, end);
+                    break;
+            }
+        }
+    };
+
+    private class BluetoothConnectThread extends Thread {
+
+        private final BluetoothSocket btSocket;
+        private final BluetoothDevice btDevice;
+
+        public BluetoothConnectThread(BluetoothDevice device) {
+            BluetoothSocket temp = null;
+            this.btDevice = device;
+            try {
+                temp = btDevice.createRfcommSocketToServiceRecord(UUID.fromString(
+                        getString(R.string.uuid)));
+            } catch (IOException e) {
+
+            }
+            this.btSocket = temp;
+        }
+
+        public void run() {
+            adapter.cancelDiscovery();
+            try {
+                btSocket.connect();
+            } catch (IOException connectException) {
+                try {
+                    btSocket.close();
+                } catch (IOException closeException) {
+
+                }
+                return;
+            }
+            connected = new BluetoothConnectedThread(btSocket);
+            connected.start();
+        }
+
+        public void cancel() {
+            try {
+                btSocket.close();
+            } catch (IOException e) {
+
+            }
+        }
+    }
+
+    private class BluetoothConnectedThread extends Thread {
+
+        private final BluetoothSocket btSocket;
+        private final InputStream inputStream;
+        private final OutputStream outputStream;
+
+        public BluetoothConnectedThread(BluetoothSocket socket) {
+            this.btSocket = socket;
+            InputStream tempInput = null;
+            OutputStream tempOutput = null;
+            try {
+                tempInput = socket.getInputStream();
+                tempOutput = socket.getOutputStream();
+            } catch (IOException e) {
+
+            }
+            this.inputStream = tempInput;
+            this.outputStream = tempOutput;
+        }
+
+        public void run() {
+            byte[] buffer = new byte[1024];
+            int bytes;
+
+            while(true) {
+                try {
+                    bytes = inputStream.read(buffer);
+                    handler.obtainMessage(Constants.MESSAGE_READ, bytes, -1, buffer).sendToTarget();
+                } catch (IOException e) {
+                    break;
+                }
+            }
+        }
+
+        public void write(byte[] bytes) {
+            try {
+                outputStream.write(bytes);
+            } catch (IOException e) {
+
+            }
+        }
+
+        public void cancel() {
+            try {
+                btSocket.close();
+            } catch (IOException e) {
+
+            }
+        }
     }
 }
